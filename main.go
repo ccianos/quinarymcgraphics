3c9e12fb61d16913f7f7070ab5c76c94dc8d2a2a
ccianos <ccianos@alphavida.com>
2025-11-10 02:56:08 -0800

    Fix arg type for flags.

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ func main() {
 	// --- CLI ---
 	outputFile := flag.String("o", "output.png", "Output PNG file name; defaults to `output.png`. Use `NONE` to disable saving.")
 	useDisplay := flag.Bool("display", true, "Display the graphic in an X window.")
-	flag.Int("width", "800", "Image width (simulation X-axis).")
-	flag.Int("height", "600", "Image height (simulation Time-axis).")
+	flag.Int("width", 800, "Image width (simulation X-axis).")
+	flag.Int("height", 600, "Image height (simulation Time-axis).")
 	flag.Parse()
 
 	fmt.Println("--- Phase 1 (text simulation): Initializing SDGA System ---")
